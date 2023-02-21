bec09b985b5d1f657dc5d9c81263ed57ded0a885
Robert L Walton <walton@acm.org>
2023-02-20 20:04:50 -0500

    update

---
+++ LSC/trailQR/Nashoba.js
@@ -0,0 +1,12 @@
+// File:	Nashoba.js
+// Author:	Bob Walton (walton@acm.org)
+// Date:	Mon Feb 20 19:53:13 EST 2023
+
+const Nashoba = {
+
+YW: "https://trails.actonma.gov/map/?zoom=15.36&lng=-71.405120&lat=42.512319&m=[{%22t%22:%22You%20Are%20Here%20(YW)%22,%22x%22:%22-71.405095%22,%22y%22:%2242.513353%22}]",
+
+YR1S: "https://trails.actonma.gov/map/?zoom=15.36&lng=-71.405120&lat=42.512319&m=[{%22t%22:%22You%20Are%20Here%20(YR1S)%22,%22x%22:%22-71.404560%22,%22y%22:%2242.512220%22}]"
+
+};
+
