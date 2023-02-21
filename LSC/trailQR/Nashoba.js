083ca81185d72778363ef710ae0e277d2f0c49f1
Robert L Walton <walton@acm.org>
2023-02-21 03:55:24 -0500

    update

--- LSC/trailQR/Nashoba.js
+++ LSC/trailQR/Nashoba.js
@@ -1,6 +1,26 @@
 // File:	Nashoba.js
 // Author:	Bob Walton (walton@acm.org)
-// Date:	Mon Feb 20 19:53:13 EST 2023
+// Date:	Tue Feb 21 03:55:03 EST 2023
+
+// R and B trails are numbered clockwise from Wheeler Road.
+//
+// YW	Wheeler Road (W).
+// YR1S	Junction Y and Carlisle Rd Access Trail (R1).
+// YR2W Junction Y and Northern Spring Hill Access Trail (R2).
+// YG1W	Junction Y and West End of Pest House Trail (G1).
+// YG2N	Junction Y and North End of Princess Pine Trail (G2).
+// YG2S	Junction Y and South End of Princess Pine Trail (G2).
+// YR3W Junction Y and Southern Spring Hill Access Trail (R3).
+// R3G3N Junction Southern Spring Hill Access Trail (R3)
+//                North End of Pipsissewa Trail (G3).
+// YB1E Junction Y and East End Blue Trail Near Davis Road (B1).
+// YD	Davis Road (D).
+// YR4E Junction Y and Rail Trail Access Trail Near Davis Road (R4).
+// YR5E Junction Y and Pencil Factory Access Trail (R5).
+// YB1W Junction Y and West End Blue Trail Near Davis Road (B1).
+// YSH  Stone House (SH) on Y.
+// YR6E Junction Y and East End of Southern Milldam Road Access Trail (R6).
+// YR7E Junction Y and East End of Northern Milldam Road Access Trail (R7).
 
 const Nashoba = {
 
